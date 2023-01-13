0de8ddb2cbc60faca85c7cfcd73fd25499ef627c
m2gi-ferrert <thibault.ferrer@etu.univ-grenoble-alpes.fr>
2023-01-13 14:35:25 +0100

    add resource heor and villain

---
+++ microservices/heroService/src/main/java/me/fjbt/workshop/supes/HeroResource.java
@@ -0,0 +1,23 @@
+package me.escoffier.workshop.supes;
+
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class HeroResource {
+
+    private static final Logger LOGGER = Logger.getLogger(HeroResource.class);
+
+    @GET
+    @Path("/heroes/random")
+    public Hero getRandomHero() {
+        Hero hero = Hero.findRandom();
+        LOGGER.debug("Found random hero " + hero);
+        return hero;
+    }
+}
